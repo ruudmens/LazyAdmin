ef35d49bd5ba7df00c1dc5b2538f251239cd1a4f
Ruud <ruud.mens@gmail.com>
2022-11-04 15:59:40 +0100

    Updated Get-MailboxStatistics to Get-EXO version

--- Office365/MailboxSizeReport.ps1
+++ Office365/MailboxSizeReport.ps1
@@ -155,7 +155,7 @@ Function Get-MailboxStats {
     $mailboxes | ForEach-Object {
 
       # Get mailbox size     
-      $mailboxSize = Get-MailboxStatistics -identity $_.UserPrincipalName | Select-Object TotalItemSize,TotalDeletedItemSize,ItemCount,DeletedItemCount,LastUserActionTime
+      $mailboxSize = Get-EXOMailboxStatistics -identity $_.UserPrincipalName | Select-Object TotalItemSize,TotalDeletedItemSize,ItemCount,DeletedItemCount,LastUserActionTime
 
       if ($null -ne $mailboxSize) {
       
